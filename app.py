ea6335c50a76f1f15972aaaf0d6001886d680062
suhai <suhailkasism02@gmail.com>
2023-02-09 13:48:41 -0500

    Create main.py

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def home():
     return "Welcome to Ott Loader."
 
+@app.route("/sync_data")
+def sync_data():
+    try:
+        return "Welcome to Ott Loader - sync_data."
+    except Exception as error:
+        print(f"Error from sync_data : {error}")
+
 if __name__ == '__main__':
     app.run( )
