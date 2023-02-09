fce049a8e40afac199db4b5ff0c27eb419a0d7c7
suhai <suhailkasism02@gmail.com>
2023-02-09 13:56:50 -0500

    Create main.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Ott Loader."
+    return render_template("home.html")
 
 @app.route("/sync_data")
 def sync_data():
